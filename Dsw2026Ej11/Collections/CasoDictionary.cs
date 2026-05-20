9866ee95aac9bb55f1efc72afcd41c523c2adbf0
Leal <lautarolealalvarado10@gmail.com>
2026-05-20 00:12:19 -0300

    Clase CasoList Completa

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un diccionario donde la clave sea el legajo y el valor el alumno
 //Incluir un método para agregar un alumno al diccionario
@@ -7,4 +9,34 @@
 //Incluir un método para eliminar un alumno utilizando la clave
 public class CasoDictionary
 {
+    Dictionary<int, Alumno> alumnos = new Dictionary<int, Alumno>();
+
+    public void AgregarAlumno(int legajo,Alumno a)
+    {
+        if (!alumnos.ContainsKey(legajo))
+        {
+            alumnos.Add(legajo, a);
+        }
+    }
+
+    public Alumno BuscarAlumno(int legajoBuscar)
+    {
+        if (alumnos.ContainsKey(legajoBuscar))
+        {
+            return alumnos[legajoBuscar];
+        }
+
+        return null; 
+    }
+
+    public Dictionary<int, Alumno> RetornarDiccionario()
+    {
+        return alumnos;
+    }
+
+    public void EliminarAlumno(int legajoEliminar)
+    {
+        alumnos.Remove(legajoEliminar);
+    }
+
 }
--- Dsw2026Ej11/Tests/Ejemplos.cs
+++ Dsw2026Ej11/Tests/Ejemplos.cs
@@ -1,4 +1,7 @@
-﻿namespace Dsw2026Ej11.Tests;
+﻿using Dsw2026Ej11.Collections;
+using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Tests;
 
 
 internal class Ejemplos
@@ -11,7 +14,31 @@ internal class Ejemplos
     //Eliminar el primer elemento de la lista y listar por consola los alumnos
     public static void EjemploList()
     {
+        CasoList cl = new CasoList();
+        Alumno a1 = new Alumno(1, "Leal", 9.5);
+        Alumno a2 = new Alumno(2, "Alicata", 8.4);
+        Alumno a3 = new Alumno(3, "Vicente", 5.7);
+        cl.AgregarAlumno(a1);
+        cl.AgregarAlumno(a2);
+        cl.AgregarAlumno(a3);
 
+        //devolver la lista
+        cl.RetornarLista();
+        Console.WriteLine("");
+
+        //buscar por nombre
+        cl.BuscarAlumno("Leal");
+
+        //alumno q no existe
+        cl.BuscarAlumno("no existe");
+
+        //eliminar alumno
+        cl.EliminarAlumno(a3);
+        cl.RetornarLista();
+
+        //eliminar 1er elemento
+        cl.EliminarAlumnoIndice(0);
+        cl.RetornarLista();
     }
 
     //Agregar 3 alumnos al diccionario
